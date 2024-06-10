d1e88edb23beaae1281960e52b087aa7ea8b84f4
Saurabh21041999 <s.loya21@gmail.com>
2024-06-09 20:59:05 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical Chatbot",
+    version='0.0.1',
+    author='Saurabh Loya',
+    author_email='s.loya21@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+)
